fdb40ba69caf84770d9561cc1add243874bfcb13
Amits <shtoberamit@gmail.com>
2016-12-06 22:14:30 +0200

    1. adding file-loader to webpack. 2. using react-modal in the Home.js

--- UChat-client/app/components/Home.js
+++ UChat-client/app/components/Home.js
@@ -6,18 +6,44 @@ var MainChatContainer = require('../containers/MainChatContainer');
 var LeftBoxes = require('../components/LeftBoxes');
 var localStorageHelpers = require('../utils/localStorageHelpersMock');
 var NotificationContainer = require('react-notifications').NotificationContainer;
+var ModalContainer = require('react-modal-dialog').ModalContainer;
+var ModalDialog = require('react-modal-dialog').ModalDialog;
+var serverIsDownImage = require('../images/server_is_down.png');
 
 class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.clearData = this.clearData.bind(this);
+        this.state = { isShowingModal: true };
+    }
+
     render() {
         var helloUser = '';
         if (localStorageHelpers.isUserLogged()) {
             helloUser = 'Hello ' + localStorageHelpers.getUser().user_nickname + '!';
         }
 
-        this.clearData = this.clearData.bind(this);
 
         return (
             <div className='container'>
+
+                <div onClick={this.handleClick}>
+                    {
+                        this.state.isShowingModal &&
+                        <ModalContainer onClose={this.handleClose}>
+                            <ModalDialog onClose={this.handleClose}>
+                                <p style={{ textAlign: 'center' }}>
+                                    <img src={serverIsDownImage} style={{ 'width': 250 + 'px' }} />
+                                </p>
+                                <h1>We are working on it.</h1>
+                            </ModalDialog>
+                        </ModalContainer>
+                    }
+                </div>
+
                 <NotificationContainer />
                 <div className="row" style={{ padding: 15 + 'px' }}>
                     <div className="page-header" >
@@ -29,6 +55,14 @@ class Home extends React.Component {
                 {this.props.children}
             </div>
         )
+    }
+
+    handleClick() {
+        this.setState({ isShowingModal: true });
+    }
+
+    handleClose() {
+        this.setState({ isShowingModal: false });
     }
 
     clearData() {
